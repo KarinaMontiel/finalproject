138a4867198ce22913158919260f5ba3fad75d41
aubrit01 <41202893+aubrit01@users.noreply.github.com>
2018-07-30 13:19:19 -0700

    zbflukqewgfxqgduj

--- aubri.js
+++ aubri.js
@@ -20,20 +20,9 @@
 //         chart.draw(data, options);
 //       }
 
-function search() {
-
-   var name = document.getElementById("searchForm").elements["searchItem"].value;
-   var pattern = name.toLowerCase();
-   var targetId = "";
-
-   var divs = document.getElementsByClassName("col-md-2");
-   for (var i = 0; i < divs.length; i++) {
-      var para = divs[i].getElementsByTagName("p");
-      var index = para[0].innerText.toLowerCase().indexOf(pattern);
-      if (index != -1) {
-         targetId = divs[i].parentNode.id;
-         document.getElementById(targetId).scrollIntoView();
-         break;
-      }
-   }
-}
+<script type="text/javascript">
+    document.getElementById('frmSearch').onsubmit = function() {
+        window.location = 'http://www.google.com/search?q=site:yoursitename.com ' + document.getElementById('txtSearch').value;
+        return false;
+    }
+</script>
